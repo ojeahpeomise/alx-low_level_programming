b0e50fe63c2ffa964031da4bb2e8518c124b1faa
Ojeah Promise Prince <ojeahpromise62@gmail.com>
2023-06-26 09:17:42 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- * main - Program that prints the instruction given in the task using put function
+ * main - Program that prints using put function
  * Return: 0
  */
 int main(void)
-{ puts("\"Programming is like building a multilingual puzzle");
+{puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
