fcf145b47de2cb9990a354f1aa82cf067d9e312b
Ojeah Promise Prince <ojeahpromise62@gmail.com>
2023-06-28 05:06:39 -0700

    0-positive_or_negative.c

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,23 @@
+#include <time.h>
+/**
+ * main - generate random numbers
+ * Return: 0
+ */
+int main(void)
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+	{
+		printf("%d is postive\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else
+	{
+		printf("%d is negative\n", n);
+	}
+	return (0);
