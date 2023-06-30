3e3b2419cb48b2a7611a05410bbae074192ad728
Ojeah Promise Prince <ojeahpromise62@gmail.com>
2023-06-30 06:10:29 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Print Combination of 2 digits
+ * Return: Always (0)
+ */
+int main(void)
+{
+	int d, p;
+
+	for (d = '0'; d < '9'; d++)
+	{
+	for (p = d + 1; p <= '9'; p++)
+	}
+}
