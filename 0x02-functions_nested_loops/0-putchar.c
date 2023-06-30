2cb05fac0e2b0e15c2897dd28d727dc03392d305
Ojeah Promise Prince <ojeahpromise62@gmail.com>
2023-06-30 09:04:28 -0700

    #include main.h

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,19 @@
 #include "main.h"
-
 /**
- * main - Enty point
- * Return: Always (0)
+ * main - print _putchar
+ * Return: 0
  */
 
 int main(void)
 {
-	_putchar(' ');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-	return (0);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
