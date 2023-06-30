418ae21132458633b25c502c8fd1b4da4a4d134f
Ojeah Promise Prince <ojeahpromise62@gmail.com>
2023-06-30 05:26:31 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Print All Single Digit Number
+ * Return: Always (0)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	printf("%d", i);
+	printf("\n");
+
+	return (0);
+}
