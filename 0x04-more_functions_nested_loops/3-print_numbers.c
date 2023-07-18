fffde57348a48f5c7b5b4714c79e6dc325351bf9
Ojeah Promise Prince <ojeahpromise62@gmail.com>
2023-07-18 12:07:23 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,15 +1,13 @@
 #include "main.h"
 /**
- * print_numbers - Print numbers from 0 up to 9
- * Return: return the numbers from 0 up to 9
+ * print_numbers - Print numbers ranging from 0 up to 9
+ * Return: return numbers 0 up to 9
  */
-
 void print_numbers(void)
 {
-	int x;
 	for (x = 0; x <= 9; x++)
 	{
-	_putchar(x + '0');
+		_putchar(x + '0');
 	}
-	_putchart('\n');
+	_putchar('\n');
 }
