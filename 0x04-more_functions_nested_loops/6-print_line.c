1e6fc98c89290c108a11f353a56ad1d46c1f69f5
Ojeah Promise Prince <ojeahpromise62@gmail.com>
2023-07-18 14:05:52 -0700

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -10,16 +10,9 @@ void print_line(int n)
 {
 	int x;
 
-	if (n <= 0)
-	{
-	_putchar('\n');
-	}
-	else
-	{
-	for (x = 0; x <= n; x++)
+	for (x = 0; x < n; x++)
 	{
 	_putchar('_');
 	}
 	_putchar('\n');
-	}
 }
