8cad4e123f572ea5f549f720c69a535969c45576
Ojeah Promise Prince <ojeahpromise62@gmail.com>
2023-07-18 14:31:33 -0700

    8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_sqaure - Function that prints a sqaure followed by a new line
+ * @size: size of square
+ */
+
+void print_sqaure(int size)
+{
+	int x, y;
+
+	if (size <= 0)
+		_putchar('\n');
+
+	for (x = 0; x < size; x++)
+	{
+		for (y = 0; y < (size); y++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
